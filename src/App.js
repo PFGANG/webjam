d6814208ad099bc39d16746aa6917011dd5f9669
Tomas Gonzalez <tgonzalezv@uc.cl>
2021-08-21 00:10:21 -0400

    Add branch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
+import { BrowserRouter as Switch, Route } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import Cloud from './views/cloud'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Switch>
+        <Route exact path="/">
+
+          <Cloud />
+
+        </Route>
+      </Switch>
     </div>
   );
 }
